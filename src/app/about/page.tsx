8098986d76c8bd3311216259ed23de34cd254178
Nanami <88866882+krmr73@users.noreply.github.com>
2024-10-01 16:32:19 +0900

    add: 中身内容の追加

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,14 @@
 const Page = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <h2>About Me</h2>
+      <div>岩橋七海</div>
+
+      <h2>Skills</h2>
+
+      <h2>Contact</h2>
+    </div>
+  );
 };
 
 export default Page;
