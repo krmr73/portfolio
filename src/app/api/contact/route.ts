2fefa5d42fac657800554e2d1820dcd3bb67b4d5
Nanami <88866882+krmr73@users.noreply.github.com>
2024-10-02 22:27:55 +0900

    fix: apiがローカルでなくても動くように

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -10,15 +10,15 @@ export async function POST(req: NextRequest) {
     const transporter = nodemailer.createTransport({
       service: "gmail", // Gmailを使用する場合
       auth: {
-        user: process.env.EMAIL_USER, // Gmailアカウントのメールアドレス
-        pass: process.env.EMAIL_PASS, // Gmailアカウントのパスワード
+        user: process.env.EMAIL_USER, // 環境変数からメールアドレスを取得
+        pass: process.env.EMAIL_PASS, // 環境変数からアプリパスワードを取得
       },
     });
 
     const mailOptions = {
-      from: email,
+      from: process.env.EMAIL_USER, // 送信元のメールアドレス (自身のGmailアカウント)
       to: process.env.EMAIL_TO, // 通知を受け取るメールアドレス
-      subject: `portfolioサイトにて、${name}からの問い合わせ・ご意見`,
+      subject: `Portfolioサイトにて、${name}からの問い合わせ・ご意見`,
       text: `下記の内容の問い合わせ・ご意見がありました。
 
       名前: ${name}
@@ -31,12 +31,12 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json(
       { message: "Email sent successfully" },
-      { status: 200 },
+      { status: 200 }
     );
   } catch (error) {
     return NextResponse.json(
       { message: "Error sending email", error },
-      { status: 500 },
+      { status: 500 }
     );
   }
 }
