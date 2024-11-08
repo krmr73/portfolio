7d0795a8e12d655106361a4a8608c9763abb2eaa
Nanami <88866882+krmr73@users.noreply.github.com>
2024-11-08 15:26:59 +0900

    add: emailに対するvalidationを追加

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,42 +1,52 @@
 import { NextRequest, NextResponse } from "next/server";
 import nodemailer from "nodemailer";
+import validator from "validator";
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
     const { name, email, message } = body;
 
+    // サニタイズとバリデーション
+    if (email && !validator.isEmail(email)) {
+      return NextResponse.json(
+        { message: "Invalid email format" },
+        { status: 400 }
+      );
+    }
+
+    const sanitizedMessage = validator.escape(message); // メッセージをエスケープ
+
     // Nodemailerの設定
     const transporter = nodemailer.createTransport({
-      service: "gmail", // Gmailを使用する場合
+      service: "gmail",
       auth: {
-        user: process.env.EMAIL_USER, // 環境変数からメールアドレスを取得
-        pass: process.env.EMAIL_PASS, // 環境変数からアプリパスワードを取得
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
 
     const mailOptions = {
-      from: process.env.EMAIL_USER, // 送信元のメールアドレス (自身のGmailアカウント)
-      to: process.env.EMAIL_TO, // 通知を受け取るメールアドレス
+      from: email,
+      to: process.env.EMAIL_TO,
       subject: `Portfolioサイトにて、${name}からの問い合わせ・ご意見`,
       text: `下記の内容の問い合わせ・ご意見がありました。
 
       名前: ${name}
       メール: ${email}
-      メッセージ: ${message}`,
+      メッセージ: ${sanitizedMessage}`,
     };
-
     // メールを送信
     await transporter.sendMail(mailOptions);
 
     return NextResponse.json(
       { message: "Email sent successfully" },
-      { status: 200 },
+      { status: 200 }
     );
   } catch (error) {
     return NextResponse.json(
       { message: "Error sending email", error },
-      { status: 500 },
+      { status: 500 }
     );
   }
 }
