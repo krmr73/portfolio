e3efdc8829eddb2c9e21b24e0bb8edfedd31f8da
Nanami <88866882+krmr73@users.noreply.github.com>
2024-11-10 19:53:07 +0900

    add: スキルに関する説明を追加

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,40 +4,64 @@ const skillCategories = [
   {
     category: "Programming Languages",
     skills: [
-      { name: "Python", level: 3 },
-      { name: "C", level: 3 },
-      { name: "C++", level: 2 },
-      { name: "Java", level: 1 },
-      { name: "JavaScript", level: 1 },
-      { name: "TypeScript", level: 1 },
-      { name: "Julia", level: 1 },
+      {
+        name: "Python",
+        level: 4,
+        description:
+          "研究に利用（数理モデル・データ分析・可視化）、授業（強化学習）",
+      },
+      { name: "C", level: 3, description: "Arduinoの電子工作に利用" },
+      { name: "C++", level: 2, description: "競技プログラミングの授業で利用" },
+      { name: "Java", level: 2, description: "授業でゲームAIの作成に利用" },
+      { name: "JavaScript", level: 2, description: "Webアプリの作成に利用" },
+      {
+        name: "TypeScript",
+        level: 2,
+        description: "webアプリの作成に利用（サマーインターン・本サイト）",
+      },
+      { name: "Julia", level: 1, description: "研究に利用" },
     ],
   },
   {
     category: "Frontend",
     skills: [
-      { name: "React", level: 1 },
-      { name: "HTML", level: 1 },
-      { name: "CSS", level: 1 },
+      {
+        name: "React",
+        level: 2,
+        description: "webアプリの作成に利用（サマーインターン・本サイト）",
+      },
+      { name: "HTML/CSS", level: 2, description: "Webアプリの作成に利用" },
     ],
   },
   {
     category: "Database",
-    skills: [{ name: "SQL", level: 1 }],
+    skills: [{ name: "SQL", level: 1, description: "授業や研究で少し利用" }],
   },
   {
     category: "Cloud Platforms",
     skills: [
-      { name: "GCP", level: 1 },
-      { name: "AWS", level: 1 },
+      {
+        name: "GCP",
+        level: 1,
+        description: "研究でCompute Engine, Cloud Storageを利用",
+      },
+      {
+        name: "AWS",
+        level: 1,
+        description: "サマーインターンでサーバ構築、データ分析を少し体験",
+      },
     ],
   },
   {
     category: "Tools",
     skills: [
-      { name: "Git", level: 2 },
-      { name: "Docker", level: 1 },
-      { name: "figma", level: 1 },
+      { name: "Git", level: 2, description: "基本的な操作が可能" },
+      { name: "Docker", level: 1, description: "研究で少し利用" },
+      {
+        name: "figma",
+        level: 2,
+        description: "論文やプレゼンに使う図の作成に利用",
+      },
     ],
   },
 ];
@@ -58,6 +82,7 @@ const Skills = () => {
             {category.skills.map((skill, index) => (
               <div key={index} className={styles.skill}>
                 <span className={styles.skillName}>{skill.name}</span>
+                <p className={styles.skillDescription}>{skill.description}</p>
                 <div className={styles.levelBar}>
                   <div
                     className={`${styles.levelIndicator} ${styles[`level${skill.level}`]}`}
@@ -71,9 +96,9 @@ const Skills = () => {
 
       <div className={styles.skillCategory}>
         <h3>Qualifications</h3>
-        <div className={styles.skillsContainer}>
+        <div className={styles.qualificationContainer}>
           {qualifications.map((qualification, index) => (
-            <div key={index} className={styles.skill}>
+            <div key={index}>
               <span className={styles.skillName}>{qualification.name}</span>
             </div>
           ))}
